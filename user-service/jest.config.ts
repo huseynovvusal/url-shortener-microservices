7bda82b5cdab2b49ef234ef399d87d422b95e6af
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-19 22:52:37 +0400

    feat: add Jest testing framework and configuration

---
+++ user-service/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@user-service/(.*)$': '<rootDir>/src/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/generated/**',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+};
+
+export default config;
