03799323995a45f92762989a43c1ddc9ecfbb4bd
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-22 11:35:58 +0400

    feat: add URL validation schemas for create, retrieve, and delete operations

---
+++ url-service/src/validations/url.schema.ts
@@ -0,0 +1,31 @@
+import { createUrlSchema } from '@user-service/dtos/create-url.dto';
+import { z } from 'zod';
+
+export const createSchema = z.object({
+  body: createUrlSchema,
+});
+
+export const getByShortCodeSchema = z.object({
+  params: z.object({
+    shortCode: z.string().min(3),
+  }),
+});
+
+export const getUrlsByUserSchema = z.object({
+  query: z.object({
+    page: z
+      .string()
+      .optional()
+      .transform((val) => (val ? parseInt(val, 10) : 1)),
+    limit: z
+      .string()
+      .optional()
+      .transform((val) => (val ? parseInt(val, 10) : 10)),
+  }),
+});
+
+export const deleteUrlSchema = z.object({
+  params: z.object({
+    id: z.string().min(1),
+  }),
+});
