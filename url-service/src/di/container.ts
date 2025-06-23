d1e8e8615df2ebe2ef1edb4b62356d5cb1e367ac
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-23 17:07:22 +0400

    refactor: move database connection logic to server startup; improve server initialization

--- url-service/src/di/container.ts
+++ url-service/src/di/container.ts
@@ -1,14 +1,11 @@
 import appConfig from '@url-service/config/app.config';
 import { createUrlController } from '@url-service/controllers/url.controller';
-import { connectDatabase } from '@url-service/helpers/connect-db';
 import { createAuthMiddleware } from '@url-service/middlewares/auth.middleware';
 import { createUrlRepository } from '@url-service/repositories/url.repository';
 import { createUrlRouter } from '@url-service/routes/url.routes';
 import { createAuthService } from '@url-service/services/auth.service';
 import { createUrlService } from '@url-service/services/url.service';
 import mongoose from 'mongoose';
-
-connectDatabase();
 
 // Repositories
 const urlRepository = createUrlRepository();
