8e0b6b318d1b529563ff0523d34f5c2ad3ee05a5
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-23 17:05:03 +0400

    feat: add database connection and error handling middleware; update dependencies

---
+++ analytics-service/src/helpers/connect-db.ts
@@ -0,0 +1,18 @@
+import dbConfig from '@url-service/config/db.config';
+import mongoose from 'mongoose';
+import { logger } from '@huseynovvusal/url-shortener-shared';
+
+export const connectDatabase = async () => {
+  mongoose
+    .connect(dbConfig.url)
+    .then((value) =>
+      logger.info(
+        `MongoDB Connection Successful: ${value.connection.host}:${value.connection.port}/${value.connection.name}`
+      )
+    )
+    .catch((error) => {
+      logger.error('MongoDB Connection Error:', error);
+
+      process.exit(1);
+    });
+};
--- analytics-service/src/server.ts
+++ analytics-service/src/server.ts
@@ -1,12 +1,27 @@
 import { logger } from '@huseynovvusal/url-shortener-shared';
 import appConfig from '@config/app.config';
 import app from '@url-service/app';
+import { connectDatabase } from './helpers/connect-db';
+import { Server } from 'node:http';
 
 const PORT = appConfig.port;
 
-const server = app.listen(PORT, () => {
-  logger.info(`User service is running on port ${PORT}`);
-});
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await connectDatabase();
+
+    server = app.listen(PORT, () => {
+      logger.info(`User service is running on port ${PORT}`);
+    });
+  } catch (error) {
+    logger.error('Error starting server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 const shutdown = async () => {
   logger.info('Shutting down server...');
