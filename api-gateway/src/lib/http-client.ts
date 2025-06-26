700daf5727910139738d5f5304904041be64ffbe
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-26 11:37:44 +0400

    feat: add HttpClient class for simplified HTTP requests and integrate axios

---
+++ api-gateway/src/lib/http-client.ts
@@ -0,0 +1,49 @@
+import { logger } from '@huseynovvusal/url-shortener-shared';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export class HttpClient {
+  private client: AxiosInstance;
+
+  constructor(baseURL: string) {
+    this.client = axios.create({
+      baseURL,
+      timeout: 5000,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    this.client.interceptors.response.use(
+      (response) => response.data,
+      (error) => {
+        logger.error(`HTTP Client Error: ${error}`);
+
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  async get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.get(url, config);
+
+    return response.data;
+  }
+
+  async post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.post(url, data, config);
+
+    return response.data;
+  }
+
+  async put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.put(url, data, config);
+
+    return response.data;
+  }
+
+  async delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.delete(url, config);
+
+    return response.data;
+  }
+}
