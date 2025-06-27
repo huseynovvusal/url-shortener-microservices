6dd4a3eca03e0488be8111c6be521d65361237d0
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-27 21:16:13 +0400

    feat: add service URLs to app configuration for improved service integration

--- api-gateway/src/config/app.config.ts
+++ api-gateway/src/config/app.config.ts
@@ -3,6 +3,9 @@ import env from '@config/env';
 const appConfig = {
   env: env.NODE_ENV,
   port: env.PORT,
+  urlServiceUrl: env.URL_SERVICE_URL,
+  userServiceUrl: env.USER_SERVICE_URL,
+  analyticsServiceUrl: env.ANALYTICS_SERVICE_URL,
   rabbitMqUrl: env.RABBITMQ_URL,
 };
 
