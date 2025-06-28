43350ed658c02b37cb7e3091063d57bd1a97d1c2
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-28 13:38:05 +0400

    feat: add proxy routes for user, URL, and analytics services; implement health check endpoint

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { errorMiddleware } from './middlewares/error.middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import appConfig from '@config/app.config';
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
 
@@ -11,9 +13,34 @@ app.use(cors());
 app.use(helmet());
 
 // Routes
+app.use(
+  '/api/users',
+  createProxyMiddleware({
+    target: appConfig.userServiceUrl,
+    changeOrigin: true,
+    pathRewrite: { '^/api/users': '/users' },
+  })
+);
+app.use(
+  '/api/urls',
+  createProxyMiddleware({
+    target: appConfig.urlServiceUrl,
+    changeOrigin: true,
+    pathRewrite: { '^/api/urls': '/urls' },
+  })
+);
+app.use(
+  '/api/analytics',
+  createProxyMiddleware({
+    target: appConfig.analyticsServiceUrl,
+    changeOrigin: true,
+    pathRewrite: { '^/api/analytics': '/analytics' },
+  })
+);
 
-// Error handler
-//@ts-ignore
-app.use(errorMiddleware);
+// Health Check Route
+app.get('/health', (_req, res) => {
+  res.status(StatusCodes.OK).json({ status: 'OK' });
+});
 
 export default app;
